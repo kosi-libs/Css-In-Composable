2b7d3a2a44f15218c7b46101ec6e34571704c1e2
Romain Boisselle <romain.boisselle@proton.me>
2024-02-03 15:28:15 +0100

    Upgrade Kotlin 1.9.21 + Compose 1.6.0-alpha01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 buildscript {
     repositories {
         mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
         maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.3.0")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.5.1")
     }
 }
 
